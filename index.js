d784d3d913efdb64ad1ae04828c0dd1eb8d0e9ef
Kinjal Vaghela <64799258+kinjalvaghela1810@users.noreply.github.com>
2024-10-12 15:57:58 +0530

    Final Commit
    JS FILE

--- index.js
+++ index.js
@@ -1,3 +1,64 @@
 let timerInterval;
 let elapsedTime = 0;
 let isRunning = false;
+
+function updateTimerDisplay() {
+  const hours = String(Math.floor(elapsedTime / 3600000)).padStart(2, "0");
+  const minutes = String(Math.floor((elapsedTime % 3600000) / 60000)).padStart(
+    2,
+    "0"
+  );
+  const seconds = String(Math.floor((elapsedTime % 60000) / 1000)).padStart(
+    2,
+    "0"
+  );
+  $(".stopwatch-timer").text(`${hours}:${minutes}:${seconds}`);
+}
+
+function displayMessage() {
+  const hours = String(Math.floor(elapsedTime / 3600000)).padStart(2, "0");
+  const minutes = String(Math.floor((elapsedTime % 3600000) / 60000)).padStart(
+    2,
+    "0"
+  );
+  const seconds = String(Math.floor((elapsedTime % 60000) / 1000)).padStart(
+    2,
+    "0"
+  );
+  $(".display-message").text(`Final Elapsed Time: ${hours}:${minutes}:${seconds}`);
+}
+
+$(".start-pause-btn").on("click", function () {
+  if (isRunning) {
+    clearInterval(timerInterval);
+    $(this).text("Resume").attr('title', 'Resume');
+  } else {
+    timerInterval = setInterval(() => {
+      elapsedTime += 1000;
+      updateTimerDisplay();
+    }, 1000);
+    $(this).text("Pause").attr('title', 'Pause');
+  }
+  isRunning = !isRunning;
+});
+
+$(".stop-btn").on("click", function () {
+  clearInterval(timerInterval);
+  isRunning = false;
+  displayMessage();
+  $(".start-pause-btn").text("Start").attr('title', 'Start');
+  setTimeout(() => {
+    elapsedTime = 0;
+    updateTimerDisplay();
+    $(".display-message").text(""); 
+  }, 3000); 
+});
+
+$(".reset-btn").on("click", function () {
+  clearInterval(timerInterval);
+  isRunning = false;
+  elapsedTime = 0;
+  updateTimerDisplay();
+  $(".display-message").text("");
+  $(".start-pause-btn").text("Start").attr('title', 'Start');
+});
